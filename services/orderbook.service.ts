4f434c262b0db54837d0a2cb199888206e90992f
Reuben Rodrigues <reubenrodrigues24@gmail.com>
2022-08-11 12:44:40 +0200

    fix orderbook

--- services/orderbook.service.ts
+++ services/orderbook.service.ts
@@ -1,10 +1,11 @@
+import { BigNumber } from "ethers";
 import { formatUnits } from "ethers/lib/utils";
 import { querySubgraph } from "../external/subgraph";
 import { OrderBook } from "../interfaces/orderbook.interface";
 
 class OrderBookService {
   public async getOrderBookFor(subgraphURL : string, symbol : string, depth: number): Promise<OrderBook> {
-    const yesterday = Math.round((new Date(new Date().getTime() - (24 * 60 * 60 * 1000))).getTime() / 1000);
+    depth = depth == 0 ? 1000 : depth;
     let data : OrderBook = {
         ticker_id: `t${symbol}-${symbol}`,
         timestamp: Math.round(Date.now() / 1000).toString(),
@@ -20,32 +21,50 @@ class OrderBookService {
             }
         `;
     let res = await querySubgraph(query, subgraphURL);
+
+    // tToken -> token , bids
     query = `
         query {        
-        tokenExchanges(where:{steakAddress:"${res.data.configs[0].steak}"}, 
+        tokenExchanges(where:{
+                steakAddress:"${res.data.configs[0].steak}",
+                soldId: 1
+            }, 
             first: ${depth / 2}, orderBy: timestamp, orderDirection: desc){
-            id
-            soldId
             tokensSold
-            boughtId
             tokensBought
-            steakAddress
             }
         } 
     `
-    res = await querySubgraph(query, subgraphURL);
-    res.data.tokenExchanges.map((x) => {
-        // TODO: check oreder here
-        if(x.soldID == 0){
-            data.bids.push([formatUnits(x.tokensBought), formatUnits(x.tokensSold)])
-        } else {
-            data.bids.push([formatUnits(x.tokensSold), formatUnits(x.tokensBought)])
-        }
-    })
+    let bidData = await querySubgraph(query, subgraphURL);
 
-    data.asks = data.bids
+    // token -> tToken , asks
+    query = `
+        query {        
+        tokenExchanges(where:{
+                steakAddress:"${res.data.configs[0].steak}",
+                soldId:0
+            }, 
+            first: ${depth / 2}, orderBy: timestamp, orderDirection: desc){
+            tokensSold
+            tokensBought
+            }
+        } 
+    `
+    let askData = await querySubgraph(query, subgraphURL);
+
+    bidData.data.tokenExchanges.map((x) => {
+        data.bids.push([calcPrice(x.tokensBought, x.tokensSold), formatUnits(x.tokensSold)])
+    })
+    askData.data.tokenExchanges.map((x) => {
+        data.asks.push([calcPrice(x.tokensBought, x.tokensSold), formatUnits(x.tokensSold)])
+    })
 
     return data
   }
+}
+
+function calcPrice(bought: number, sold: number) {
+    const TEN_POW_18 = BigNumber.from(10).pow(18)
+    return  formatUnits(BigNumber.from(bought).mul(TEN_POW_18).div(sold))
 }
 export default OrderBookService;
