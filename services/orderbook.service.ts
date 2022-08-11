8b0d915a382394e6293266268cc8ff5cfd243e7e
Reuben Rodrigues <reubenrodrigues24@gmail.com>
2022-08-11 14:19:02 +0200

    Pagination for orderbook

--- services/orderbook.service.ts
+++ services/orderbook.service.ts
@@ -5,7 +5,6 @@ import { OrderBook } from "../interfaces/orderbook.interface";
 
 class OrderBookService {
   public async getOrderBookFor(subgraphURL : string, symbol : string, depth: number): Promise<OrderBook> {
-    depth = depth == 0 ? 1000 : depth;
     let data : OrderBook = {
         ticker_id: `t${symbol}-${symbol}`,
         timestamp: Math.round(Date.now() / 1000).toString(),
@@ -22,40 +21,62 @@ class OrderBookService {
         `;
     let res = await querySubgraph(query, subgraphURL);
 
+    // TODO: Write general purpose wrapper for pagination support
     // tToken -> token , bids
-    query = `
-        query {        
-        tokenExchanges(where:{
-                steakAddress:"${res.data.configs[0].steak}",
-                soldId: 1
-            }, 
-            first: ${depth / 2}, orderBy: timestamp, orderDirection: desc){
-            tokensSold
-            tokensBought
-            }
-        } 
-    `
-    let bidData = await querySubgraph(query, subgraphURL);
+    let bidData = []
+    let askData = []
+    let returnedData
+    let skip = 0
+
+    do{
+        query = `
+            query {        
+            tokenExchanges(where:{
+                    steakAddress:"${res.data.configs[0].steak}",
+                    soldId: 1
+                }, 
+                first: 100, skip: ${skip}, orderBy: timestamp, orderDirection: desc){
+                tokensSold
+                tokensBought
+                }
+            } 
+        `
+        returnedData = (await querySubgraph(query, subgraphURL)).data.tokenExchanges
+        bidData = bidData.concat(returnedData)
+        skip += 100
+    } while (returnedData.length > 0)
 
     // token -> tToken , asks
-    query = `
-        query {        
-        tokenExchanges(where:{
-                steakAddress:"${res.data.configs[0].steak}",
-                soldId:0
-            }, 
-            first: ${depth / 2}, orderBy: timestamp, orderDirection: desc){
-            tokensSold
-            tokensBought
-            }
-        } 
-    `
-    let askData = await querySubgraph(query, subgraphURL);
+    skip = 0
+    do{
+        query = `
+            query {        
+            tokenExchanges(where:{
+                    steakAddress:"${res.data.configs[0].steak}",
+                    soldId: 0
+                }, 
+                first: 100, skip: ${skip}, orderBy: timestamp, orderDirection: desc){
+                tokensSold
+                tokensBought
+                }
+            } 
+        `
+        returnedData = (await querySubgraph(query, subgraphURL)).data.tokenExchanges
+        askData = askData.concat(returnedData)
+        skip += 100
+    } while (returnedData.length > 0)
 
-    bidData.data.tokenExchanges.map((x) => {
+    // TODO: Optimize by only fetching what is required
+    if(depth > 0) {
+        bidData = bidData.slice(0, depth / 2)
+        askData = askData.slice(0, depth / 2)
+    }
+    
+
+    bidData.map((x) => {
         data.bids.push([calcPrice(x.tokensBought, x.tokensSold), formatUnits(x.tokensSold)])
     })
-    askData.data.tokenExchanges.map((x) => {
+    askData.map((x) => {
         data.asks.push([calcPrice(x.tokensBought, x.tokensSold), formatUnits(x.tokensSold)])
     })
 
